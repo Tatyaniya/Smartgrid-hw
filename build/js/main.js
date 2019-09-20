77a0f8a240772f3c4d5dcc2d659b3afe4077d7cd
Tatyaniya <tatyaniya@bigmir.net>
2019-09-20 15:44:13 +0300

    js

--- build/js/main.js
+++ build/js/main.js
@@ -8,14 +8,28 @@ $( function() {
           });
     });
 
-    $('.menu__link').click( ()=> {
+    $('.menu__link').click( function() {
         $('.nav__menu').animate({
-            display: "block",
-            height: "toggle"
-          }, 500, function() {
+                display: "block",
+                height: "toggle"
+            }, 500, function() {
           });
         $('.ham').removeClass('active');
     });
+
+
+    $(window).resize(function(e){
+
+        if ($(window).width() >= '768'){
+            $('.nav__menu').show(5);
+                return this;}
+            else {
+                $('.nav__menu').hide(5);
+            }
+    });
+    
+    $(window).resize();
+
 
     $('.menu__link').click( function(e) {
         e.preventDefault();
--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ $( function() {
           });
     });
 
-    $('.menu__link').click( ()=> {
+    $('.menu__link').click( function() {
         $('.nav__menu').animate({
                 display: "block",
                 height: "toggle"
@@ -16,6 +16,20 @@ $( function() {
           });
         $('.ham').removeClass('active');
     });
+
+
+    $(window).resize(function(e){
+
+        if ($(window).width() >= '768'){
+            $('.nav__menu').show(5);
+                return this;}
+            else {
+                $('.nav__menu').hide(5);
+            }
+    });
+    
+    $(window).resize();
+
 
     $('.menu__link').click( function(e) {
         e.preventDefault();
